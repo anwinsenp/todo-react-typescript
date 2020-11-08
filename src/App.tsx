366100ed31cd8a9a600e4fe9fe3ade48c74b9ad7
Anwin Sen Padma <anwinsenp@gmail.com>
2020-11-08 07:04:21 +0530

    Adds saveTodo to App.tsx
    Signed-off-by: Anwin Sen Padma <anwinsenp@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,18 @@ const App: React.FC = () => {
       .then(({ data: { todos } }: ITodo[] | any) => setTodos(todos))
       .catch((err: Error) => console.log(err))
   }
+
+  const handleSaveTodo = (e: React.FormEvent, formData: ITodo): void => {
+    e.preventDefault()
+    addTodo(formData)
+      .then(({ status, data }) => {
+        if (status !== 201) {
+          throw new Error("Error! Todo not saved")
+        }
+        setTodos(data.todos)
+      })
+      .catch(err => console.log(err))
+  }
 }
 
 export default App;
